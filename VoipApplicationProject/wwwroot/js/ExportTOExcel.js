bb5142b990b90c6be51c9a778f64987ed1917d2d
Lucky786 <55803417+Lucky786@users.noreply.github.com>
2021-12-02 16:00:01 +0530

    Added Export To Excel Fuctionality

---
+++ VoipApplicationProject/wwwroot/js/ExportTOExcel.js
@@ -0,0 +1,23 @@
+﻿function fnExcelReport(id, name) {
+    var title = $("#title").val();
+    var tab_text = '<html xmlns: x="urn:schemas-microsoft-com:office:excel">';
+    tab_text = tab_text + '<head><xml><x: ExcelWorkbook><x: ExcelWorksheets><x: ExcelWorksheet>';
+    tab_text = tab_text + '<x: Name>Data Sheet</x: Name>';
+    tab_text = tab_text + '<x: WorksheetOptions><x: Panes></x: Panes></x: WorksheetOptions ></x: ExcelWorksheet > ';
+    tab_text = tab_text + '</x:ExcelWorksheets></x:ExcelWorkbook></xml></head><body>';
+    tab_text = tab_text + "<table border='0px' style='color:green'>";
+
+
+    var exportTable = $('#' + id).clone();
+    exportTable.find('th').last().remove();
+    exportTable.find('input').each(function (index, elem) { $(elem).remove(); });
+    exportTable.find('a').each(function (index, elem) { $(elem).remove(); });
+
+    tab_text = tab_text + exportTable.html();
+    tab_text = tab_text + '</table></body></html>';
+    var fileName = title +'Table' + '.xls';
+
+    //Save the file
+    var blob = new Blob([tab_text], { type: "application/vnd.ms-excel;charset=utf-8" })
+    window.saveAs(blob, fileName);
+}
