53516f2d3b85fd1ffd66d7b433adc8bb7131f4ac
Lucky786 <55803417+Lucky786@users.noreply.github.com>
2021-12-02 12:02:38 +0530

    Update - View Files, Logout Functionality, Entities- SubUser, CallRates

---
+++ VoipApplicationProject/Models/CallRatesModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VoipApplicationProject.Models
+{
+    public class CallRatesModel
+    {
+        public Country Country { get; set; }
+        public double Callrates { get; set; }
+    }
+    public enum Country
+    {
+        India = 0,
+        Pakistan = 1,
+        Srilanka = 2,
+        Brazil = 3,
+        China = 4,
+        Japan = 5,
+        Afghanistan = 6,
+        Russia = 7,
+        Nepal = 8
+    }
+}
---
+++ VoipApplicationProject/Models/SubUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VoipApplicationProject.Models
+{
+    public class SubUser
+    {
+        public int SrNo { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string CalledID { get; set; }
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public SubUserType SubUserTypeID { get; set; }
+
+
+    }
+    public enum SubUserType
+    {
+        Agent = 0,
+        Device = 1,
+    }
+}
